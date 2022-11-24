0f799d04f38805c6b163726588a80fcd1762373c
Robert812218 <robert812@shaw.ca>
2022-11-23 19:41:42 -0700

    update

--- src/main/frontend/app.cljs
+++ src/main/frontend/app.cljs
@@ -1,4 +1,17 @@
-(ns frontend.app)
+(ns frontend.app
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
 
 (defn init []
-  (println "Hello World"))
+  (rdom/render
+   [simple-component]
+   (.-body js/document)))
+
--- src/torquingtony/frontend/app.cljs
+++
@@ -1,5 +0,0 @@
-(ns acme.frontend.app)
-
-(defn init []
-    (println "Hello World"))
-
